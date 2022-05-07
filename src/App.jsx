c85cbeaa2853a5b169b0ccca27839a007d3174c2
Julian Martinez <julian.fmartinez@gmail.com>
2022-05-07 18:32:55 -0300

    add: se agrega README
    Primer entrega proyecto final lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Header from './components/Header';
 import Header from './components/Header';
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import Error from './components/404';
 function App() {
 
   return (
@@ -16,10 +16,10 @@ function App() {
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:id" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       <Footer />
       </BrowserRouter>
-      {/* <ItemListContainer /> */}
     </>
   );
 }
